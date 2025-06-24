62f6e668b075b7c718855d76e76d70df6e4e530d
siewchoo <snowpod@gmail.com>
2025-06-25 02:02:05 +0800

    Updates to shorten the letter

--- public/OntologyOne/config.js
+++ public/OntologyOne/config.js
@@ -1,5 +1,5 @@
 window.APP_LINKS = {
   app: "https://ontologyone-frontend-qm41.onrender.com/?tab=app",
   chatbot: "https://ontologyone-frontend-qm41.onrender.com/?tab=chatbot",
-  blog: "https://thinklog.netlify.app",
+  blog: "https://loopbirb.netlify.app",
 };
