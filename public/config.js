4529babe9222afbd63b6481442a74c996f461310
siewchoo <snowpod@gmail.com>
2025-07-03 09:40:46 +0800

    Add logtail/betterstack logging

--- public/config.js
+++ public/config.js
@@ -1,5 +1,7 @@
 window.APP_LINKS = {
   blog: "https://loopbirb.netlify.app",
-  app: "https://ontologyone-frontend-qm41.onrender.com/?tab=app",
+  app: "https://ontologyone-frontend.onrender.com/?tab=app",
+  app1: "https://ontologyone-frontend-qm41.onrender.com/?tab=app",
   chatbot: "https://ontologyone-frontend-qm41.onrender.com/?tab=chatbot",
+  chatbot1: "https://ontologyone-frontend.onrender.com/?tab=chatbot",
 };
